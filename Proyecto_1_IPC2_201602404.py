d0ec5409e85f46349d897d6eec903df245b996aa
BlackTaro1 <tarosecaida@gmail.com>
2021-08-04 11:44:12 -0600

    Create Proyecto_1_IPC2_201602404.py
    Intento 1 crear el menu, el programa entro a un bucle infinito.

---
+++ Proyecto_1_IPC2_201602404.py
@@ -0,0 +1,23 @@
+def Menu():
+        opcion = 0 
+        while opcion != 2:
+             print('----- Menu Principal -----')
+        print('1. Cargar archivo.')
+        print('2. Procesar archivo')
+        print('3. Escribir archivo de salida')
+        print('4. Mostrar datos del estudiante')
+        print('5. Generar gráfica')     
+        print('6. Salir')
+        opcion = input()
+
+        if opcion == '1':
+            print('* Kevin Estuardo Secaida Molina')
+            print('* 201602404')
+            print('* Introducción a la programación y computación 2 sección "D"')
+            print('* Ingenieria en Ciencias y Sistemas')
+            print('* 4to. Semestre')         
+        else:
+            opcion = 2
+
+Menu() 
+   

3d2a6a3b8cc8049ed34dc460352aa9a3730ea6a3
BlackTaro1 <tarosecaida@gmail.com>
2021-08-05 10:36:48 -0600

    Update Proyecto_1_IPC2_201602404.py
    Se realizo el menu con exito.

--- Proyecto_1_IPC2_201602404.py
+++ Proyecto_1_IPC2_201602404.py
@@ -1,22 +1,24 @@
+def ImprimirDtEst():
+    print('* Kevin Estuardo Secaida Molina')
+    print('* 201602404')
+    print('* Introducción a la programación y computación 2 sección "D"')
+    print('* Ingenieria en Ciencias y Sistemas')
+    print('* 4to. Semestre')         
+
 def Menu():
-        opcion = 1
+        opcion = 0
         while opcion != 2:
             print('----- Menu Principal -----')
             print('1. Cargar archivo.')
             print('2. Procesar archivo')
-            print('3. Escribir archivo de salida')
-            print('4. Mostrar datos del estudiante')
-            print('5. Generar gráfica')     
-            print('6. Salir')
-            break
-        opcion = input()
-        ruta = ''
-        if opcion == 1:
-            print('* Kevin Estuardo Secaida Molina')
-            print('* 201602404')
-            print('* Introducción a la programación y computación 2 sección "D"')
-            print('* Ingenieria en Ciencias y Sistemas')
-            print('* 4to. Semestre')         
-        else:
-            opcion = 2
+            #print('3. Escribir archivo de salida')
+            #print('4. Mostrar datos del estudiante')
+            #print('5. Generar gráfica')     
+            #print('6. Salir')
+            opcion = input()
+            if opcion == '1':
+                ImprimirDtEst()
+                opcion = 1
+            else:
+                opcion = 2
 Menu()

63ec86cf28e4648d7795c1aa2a88a1d455c652d6
BlackTaro1 <tarosecaida@gmail.com>
2021-08-05 21:41:06 -0600

    Pas3
    Se agrego un archivo XML para poder validar la opción 1

--- Proyecto_1_IPC2_201602404.py
+++ Proyecto_1_IPC2_201602404.py
@@ -6,11 +6,19 @@ def ImprimirDtEst():
     print('* 4to. Semestre \n')  
 
 def CargarArch():
-    print('* Kevin Estuardo Secaida Molina')
-    print('* 201602404')
-    print('* Introducción a la programación y computación 2 sección "D"')
-    print('* Ingenieria en Ciencias y Sistemas')
-    print('* 4to. Semestre \n')         
+    from xml.etree.ElementTree import parse, Element
+    nombre_arch = 'Taro.xml'
+    doc_xml = parse(nombre_arch)
+    raiz = doc_xml.getroot()
+    print(raiz)
+    
+    print ()
+
+    #print('* Kevin Estuardo Secaida Molina')
+    #print('* 201602404')
+    #print('* Introducción a la programación y computación 2 sección "D"')
+    #print('* Ingenieria en Ciencias y Sistemas')
+    #print('* 4to. Semestre \n')         
 
 def Menu():
           opcion = 0
@@ -19,16 +27,19 @@ def Menu():
             print('1. Cargar archivo.')
             print('2. Procesar archivo')
             #print('3. Escribir archivo de salida')
-            #print('4. Mostrar datos del estudiante')
+            print('4. Mostrar datos del estudiante')
             #print('5. Generar gráfica')     
             print('6. Salir')
             opcion = input()
+            ruta = ''
             if opcion == '1':
+                print('El archivo a cargar es: ', ruta)
+                
+                CargarArch()
+            elif opcion == '4':
                 print('Los datos del estudiante son: \n')
                 ImprimirDtEst()
-            elif opcion == '2':
-                print('vale verga la vida')
-                CargarArch()
+
             else:
                 opcion = 6
 Menu()

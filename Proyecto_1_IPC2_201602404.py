aa5d119beab6e67701b0c976e45b177c1dbdba66
BlackTaro1 <tarosecaida@gmail.com>
2021-08-07 10:33:46 -0600

    Update Proyecto_1_IPC2_201602404.py
    element tree agregado, sigue el problema de lectura del archivo.

--- Proyecto_1_IPC2_201602404.py
+++ Proyecto_1_IPC2_201602404.py
@@ -1,3 +1,6 @@
+from xml.etree.ElementTree import TreeBuilder
+
+
 def ImprimirDtEst():
     print('* Kevin Estuardo Secaida Molina')
     print('* 201602404')
@@ -5,27 +8,18 @@ def ImprimirDtEst():
     print('* Ingenieria en Ciencias y Sistemas')
     print('* 4to. Semestre \n')  
 
-def CargarArch():
-    #from xml.etree.ElementTree import parse, Element
-    #nombre_arch = 'Taro.xml'
-    #doc_xml = parse(nombre_arch)
-    #raiz = doc_xml.getroot()
-    #print(raiz)
-    
-    #print ()
-
-    print('* Kevin Estuardo Secaida Molina')
-    #print('* 201602404')
-    #print('* Introducción a la programación y computación 2 sección "D"')
-    #print('* Ingenieria en Ciencias y Sistemas')
-    #print('* 4to. Semestre \n')         
+def CargarArch(ruta):
+    import xml.etree.ElementTree as ET
+    tree = ET.parse(ruta)
+    raiz = tree.getroot()
+    print('* Kevin Estuardo Secaida Molina', raiz)   
 
 def Menu():
           opcion = 0
           while opcion != 6:
             print('----- Menu Principal -----')
             print('1. Cargar archivo.')
-            print('2. Procesar archivo')
+            #print('2. Procesar archivo')
             #print('3. Escribir archivo de salida')
             print('4. Mostrar datos del estudiante')
             #print('5. Generar gráfica')     
@@ -34,7 +28,7 @@ def Menu():
             ruta = ''
             if opcion == '1':
                 print('El archivo a cargar es: ', ruta)
-                
+                ruta = input()
                 CargarArch()
             elif opcion == '4':
                 print('Los datos del estudiante son: \n')
